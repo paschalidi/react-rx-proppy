bc5783d1cce8296f40ec62925cdf7c33d7a44e89
christos <paschalidi@outlook.com>
2019-05-05 14:09:37 +0200

    App

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { attach } from "proppy-react";
+import { withStore } from "proppy-redux";
+import { compose, withHandlers, didSubscribe } from "proppy";
+import * as types from "../infra/types";
+import dispatch from "../infra/dispatch";
 
-function App() {
+const P = compose(
+  withStore(state => ({ appReducer: state.appReducer }), {
+    dispatch
+  }),
+  withHandlers({
+    onSyncAction: props => () => props.dispatch(types.SYNC_ACTION),
+    onAsyncAction: props => () => props.dispatch(types.ASYNC_ACTION)
+  }),
+  didSubscribe(props => props.onSyncAction(), props => props.onAsyncAction())
+);
+
+function MyComponent({ onSyncAction, onAsyncAction, appReducer, counter }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>ProppyJS: withStream</h2>
+      <div className="App">
+        <div className="App-heading App-flex">
+          <h2>
+            Welcome to <span className="App-react">react-rx</span>
+          </h2>
+        </div>
+        <div className="App-instructions App-flex">
+          <button onClick={onSyncAction}>
+            click here to dispatch a sync action
+          </button>
+          <button onClick={onAsyncAction}>
+            click here to dispatch an async action
+          </button>
+        </div>
+        <div>{counter}</div>
+        {appReducer.data && <div>data has been fetched</div>}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default attach(P)(MyComponent);
