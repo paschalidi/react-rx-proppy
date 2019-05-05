7db9ff79b791cc8ddfc88c4f6b01912e45053b1d
christos <paschalidi@outlook.com>
2019-05-05 14:09:53 +0200

    the index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { ProppyProvider } from "proppy-react";
+import App from "./app/App";
+import * as serviceWorker from "./serviceWorker";
+import configureStore from "./infra/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const providers = { store: configureStore() };
+
+ReactDOM.render(
+  <ProppyProvider providers={providers}>
+    <App property="i am a property" />
+  </ProppyProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
